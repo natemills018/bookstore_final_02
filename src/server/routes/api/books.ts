787108178e8c07b5ee34bb3b7c3da2fa7e55aad7
Nate Mills <natemills018@gmail.com>
2024-07-19 13:29:33 -0400

    Completed writing router code for books and categories

--- src/server/routes/api/books.ts
+++ src/server/routes/api/books.ts
@@ -16,4 +16,62 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    try {
+            const id = parseInt(req.params.id, 10)
+            const [book] = await db.books.getOne(id)
+            
+            if(!book) {
+                return res.status(404).json({ message: 'No book found at this id'})
+            }
+            res.json(book)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: 'Internal Server Error, could not retrieve book', error})
+    }
+})
+
+
+router.delete('/:id', async (req, res) => {
+    try {
+         const id = parseInt(req.params.id, 10)
+         const deletedBook = req.body;
+         console.log({ id, bookData: deletedBook})
+         await db.books.deleteBook(id)
+         res.json({ message: `The book at id number ${id} has been wiped off of the server`})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: 'Internal Server error, could not delete book', error})
+    }
+})
+
+router.put('/:id', tokenCheck, async(req, res) => {
+    try {
+        const id = parseInt(req.params.id, 10)
+        const updatedBook = req.body;
+        await db.books.updatedBook(updatedBook, id);
+        res.json({message: `You have update the book at id ${id}`})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: 'Internal Server error, could not update book', error})
+    }
+})
+
+router.post('/id', tokenCheck, async(req, res) => {
+    try {
+        const {title, price, author, category_id} = req.body
+
+        if(!title || !price || !author || !category_id) {
+            return res.status(400).json({ message: 'You must be missing one of the required properties'})
+        }
+        const results = await db.books.addBook({ title, price, author, category_id})
+        res.status(201).json({ message: 'Yes! We added your book to the bookstore'})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: 'Internal Server error, could not add book', error})
+    }
+})
+
 export default router;
+
+
--- src/server/routes/api/categories.ts
+++ src/server/routes/api/categories.ts
@@ -1,5 +1,16 @@
 import { Router } from "express";
+import db from "../../db";
 
 const router = Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const categories = await db.categories.getAll()
+        res.json(categories);
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'Internal Server Error, could not retrieve categories', error})
+    }
+})
 
 export default router;
